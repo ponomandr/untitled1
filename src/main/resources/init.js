ceb08201021d8720666d85f36fa3c05056bb3392
Andrey Ponomarev <ponomandr@gmail.com>
2015-05-30 23:55:59 +0200

    .

--- src/main/resources/init.js
+++ src/main/resources/init.js
@@ -1,9 +1,19 @@
+Object.defineProperty(Object.prototype, "__defineGetter__", {
+    configurable: true,
+    enumerable: false,
+    writable: true,
+    value: function (name, func) {
+        Object.defineProperty(this, name, {configurable: true, enumerable: true, get: func});
+    }
+});
+
 var natives = {};
 for (var i = 0; i < nativeModuleList.length; i++) {
     var fileName = nativeModuleList[i];
     var moduleName = fileName.substr(0, fileName.lastIndexOf('.js'));
     natives[moduleName] = __Main.readResource('/lib/' + fileName);
 }
+natives.config = "#\n{}";
 
 var processFunction = load(__Main.getResource('/process.js'));
 var process = processFunction(natives);
